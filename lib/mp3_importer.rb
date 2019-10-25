6c4a02f73690eaa167b4767cbabf338da6b8e07a
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-25 03:57:25 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ class MP3Importer
 
   def files
     # @files = Dir["#{@path}/*.mp3"]
-    @files = Dir.entries(@path)
+    @files = Dir.entries(@path).select {|f| !File.directory? f}
     binding.pry
   end
 
