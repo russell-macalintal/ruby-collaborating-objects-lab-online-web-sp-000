a471c68754745455e87dd423f7bc9736db441177
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-25 03:35:51 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,5 @@
 class MP3Importer
+  require 'pry'
   attr_accessor :path
 
   def initialize(filepath)
@@ -7,6 +8,7 @@ class MP3Importer
 
   def files
     @files = Dir['#{@path}*.mp3'].select {|e| File.file?(e)}
+    binding.pry
   end
 
   def import
